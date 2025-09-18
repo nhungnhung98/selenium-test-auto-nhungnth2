02b69bfacf731a404d3f148d5217b737875cc8a2
Nguyen Hong Nhung <hongnhung04091998@gmail.com>
2025-09-18 10:19:40 +0700

    update bai tap Locator

--- src/test/java/webdriver/Topic01_Locator.java
+++ src/test/java/webdriver/Topic01_Locator.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -34,11 +35,77 @@ public class Topic01_Locator {
         driver.findElement(By.name("login[password]"));
         driver.findElement(By.name("q"));
     }
+
     @Test
     public void TC_04_tagName() {
         //Tim ra co bao nhieu the HTMl giong nhau
-        int inputNumber=driver.findElements(By.tagName("input")).size();
+        int inputNumber = driver.findElements(By.tagName("input")).size();
         System.out.println(inputNumber);
     }
 
+    @Test
+    public void TC_05_linkText() {
+        driver.findElement(By.linkText("MY ACCOUNT"));
+        driver.findElement(By.linkText("ORDERS AND RETURNS"));
+    }
+
+    @Test
+    public void TC_06_partial_linkText() {
+        driver.findElement(By.partialLinkText("ACCOUNT"));
+        driver.findElement(By.partialLinkText("RETURNS"));
+    }
+
+    @Test
+    public void TC7_0_Css() {
+        //Css ID
+        driver.findElement(By.cssSelector("input[id='email']"));
+        driver.findElement(By.cssSelector("#email"));
+        driver.findElement(By.cssSelector("input#email"));
+
+        //Css Class
+        driver.findElement(By.cssSelector("div[class='account-login']"));
+        driver.findElement(By.cssSelector(".account-login"));
+        driver.findElement(By.cssSelector("div.account-login"));
+
+        //Css Name
+        driver.findElement(By.cssSelector("input[name='login[username]']"));
+
+        //Css tagName
+        driver.findElement(By.cssSelector("input"));
+
+        // Css Link
+        driver.findElement(By.cssSelector("a[href='http://live.techpanda.org/index.php/customer/account/']"));
+
+        // Css Partial Link
+        driver.findElement(By.cssSelector("a[href*='customer/account/']"));
+    }
+
+    @Test
+    public void TC_08_Xpath() {
+        // xpath -ID
+        driver.findElement(By.xpath("//input[@id='email']"));
+
+        // xpath-class
+        driver.findElement(By.xpath("//div[@class='account-login']"));
+        driver.findElement(By.xpath("//div[@class='col-2 registered-users']"));
+
+        // xpath-name
+        driver.findElement(By.xpath("//input[@name='login[username]']"));
+
+        // xpath-tagName
+        driver.findElement(By.xpath("//input"));
+
+        // xpath-Link
+        driver.findElement(By.xpath("//a[@href='http://live.techpanda.org/index.php/customer/account/']"));
+        driver.findElement(By.xpath("//a[text()='My Account']"));
+
+        // xpath-Partial Link
+        driver.findElement(By.xpath("//a[contains(@href,'customer/account/')]"));
+        driver.findElement(By.xpath("//a[contains(text(),'My Account')]"));
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
 }
