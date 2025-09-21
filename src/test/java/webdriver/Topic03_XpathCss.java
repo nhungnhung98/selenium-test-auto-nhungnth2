4910580a3dfe5491cd21c0baa782fdb390f79e75
Nguyen Hong Nhung <hongnhung04091998@gmail.com>
2025-09-21 22:56:51 +0700

    update bai tap Xpath

--- src/test/java/webdriver/Topic03_XpathCss.java
+++ src/test/java/webdriver/Topic03_XpathCss.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,12 +19,11 @@ public class Topic03_XpathCss {
 
     @BeforeClass
     public void beforeClass() {
-        driver = new ChromeDriver();
-        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+        driver = new FirefoxDriver();
         }
-    @Test
+    @Test //Tim Xpath theo cac locator khac nhau
     public void TC_01() {
-        //Tim Xpath theo cac locator khac nhau
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
         driver.findElement(By.xpath("//input[@value='M']"));
         driver.findElement(By.xpath("//input[@id='FirstName']"));
         driver.findElement(By.xpath("//input[@name='LastName']"));
@@ -33,7 +33,24 @@ public class Topic03_XpathCss {
         driver.findElement(By.xpath("//input[@name='Password']"));
         driver.findElement(By.xpath("//input[@name='ConfirmPassword']"));
     }
+    @Test
+    public void TC_02_PhuDinh()
+    {
+        driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+        driver.findElement(By.xpath("//a[text()='18']")).click();
+        driver.findElement(By.xpath("//div[not(@style='display:none;')]/div[@class='raDiv']"));
+    }
+    @Test
+    public void TC_03_startsWith() throws InterruptedException {
+        driver.get("https://www.lazada.vn/#?");
+        driver.findElement(By.xpath("//a[text()='login']")).click();
+//        Thread.sleep(3000);
+////        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+////        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
+////        element.sendKeys("admin");
+        driver.findElement(By.xpath("//input[starts-with(@data-spm-anchor-id,'a2o4n.homepage')]"));
 
+    }
 
     @AfterClass
     public void afterClass() {
