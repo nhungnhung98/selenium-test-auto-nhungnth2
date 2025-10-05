6d2d94735a749b4486ac8ea0563a99afdb5d1b6a
Nguyen Hong Nhung <hongnhung04091998@gmail.com>
2025-10-05 23:09:53 +0700

    update bai tap demo Xpath/Css axes

--- src/test/java/webdriver/Topic03_XpathCss.java
+++ src/test/java/webdriver/Topic03_XpathCss.java
@@ -19,7 +19,7 @@ public class Topic03_XpathCss {
 
     @BeforeClass
     public void beforeClass() {
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         }
     @Test //Tim Xpath theo cac locator khac nhau
     public void TC_01() {
@@ -49,6 +49,37 @@ public class Topic03_XpathCss {
 ////        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
 ////        element.sendKeys("admin");
         driver.findElement(By.xpath("//input[starts-with(@data-spm-anchor-id,'a2o4n.homepage')]"));
+    }
+    @Test
+    public void TC_08_axes() {
+        driver.get("https://live.techpanda.org/index.php/mobile.html");
+
+        //từ node con lên note tổ tiên
+        driver.findElement(By.xpath("//a[text()='Sony Xperia']/parent::h2/ancestor::div[@class='main']"));
+
+        // từ node con lên node cha: parent
+        // node em đi lên node anh: preceding-sibling
+        //node anh đi xuống node em: following-sibling
+        driver.findElement(By.xpath("//a[text()='IPhone']/parent::h2/following-sibling::div[@class='actions']/button"));
+        driver.findElement(By.xpath("//a[text()='Sony Xperia']/parent::h2/following-sibling::div[@class='actions']/button"));
+
+        // đi lên node cha k cần dùng parent: /.. = parent::div
+        driver.findElement(By.xpath("//a[text()='Sony Xperia']/parent::h2/following-sibling::div[@class='actions']/.."));
+        driver.findElement(By.xpath("//a[text()='Sony Xperia']/parent::h2/following-sibling::div[@class='actions']/parent::div"));
+
+        //----------------------------
+
+        driver.get("https://subscription.packtpub.com/search?query=Selenium&products=Book%20%2B%20AI%20Assistant~Book&released=Available~Early%20Access");
+        driver.findElement(By.xpath("//div[text()='Selenium Essentials']/ancestor::a/following-sibling::a//button[@class='cart-btn']"));
+        driver.findElement(By.xpath("//div[text()='Learning Selenium Testing Tools - Third Edition']/ancestor::a/following-sibling::a//button[@class='cart-btn']"));
+
+        //----------------------------
+        driver.get("https://kajabi.com/pricing");
+        driver.findElement(By.xpath("//h2[text()='Kickstarter']//parent::div/following-sibling::a[@id='pricing-kickstarter-annual']"));
+        //----------------------------
+
+        driver.get("https://demo.guru99.com/");
+        driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td"));
     }
 
     @AfterClass
