b0bdfc1c35fff7f3ba3b5430fdbf4021fe36f14b
Nguyen Hong Nhung <hongnhung04091998@gmail.com>
2025-10-11 22:58:19 +0700

    Bai tap Element Mailchimp

--- src/test/java/webdriver/Topic07_ElementExcercise.java
+++ src/test/java/webdriver/Topic07_ElementExcercise.java
@@ -18,34 +18,123 @@ public class Topic07_ElementExcercise {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
-    //bai tap: https://docs.google.com/document/d/1G0QsiVDI5KfhNGKxE6a03bhn6KpvgDh69BTx2ttCO94/edit?tab=t.0
+
+    //Link bai tap: https://docs.google.com/document/d/1G0QsiVDI5KfhNGKxE6a03bhn6KpvgDh69BTx2ttCO94/edit?tab=t.0
     @Test
     public void TC_01_Displayed_Enable_Selected() {
         driver.get("https://www.fahasa.com/customer/account/create/");
 
-    //Element display: co the nhin thay, thao tac duoc, kich thuoc cu the
-    Assert.assertTrue(driver.findElement(By.cssSelector("input#register_phone")).isDisplayed());
-    Assert.assertTrue(driver.findElement(By.cssSelector("input#register_phone_otp")).isDisplayed());
-    Assert.assertTrue(driver.findElement(By.cssSelector("button.fhs-btn-register")).isDisplayed());
+        //Element display: co the nhin thay, thao tac duoc, kich thuoc cu the
+        // Mong doi 1 element hien thi
+        Assert.assertTrue(driver.findElement(By.cssSelector("input#register_phone")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("input#register_phone_otp")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("button.fhs-btn-register")).isDisplayed());
 
-    //Element Enable: co the thao tac dc
+        //Mong doi 1 element khong hien thi
+        Assert.assertFalse(driver.findElement(By.cssSelector("input#login_username")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("input#login_password")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("button.fhs-btn-login")).isDisplayed());
 
+        //Element Enable: co the thao tac dc, khong bi read only=disable
+        Assert.assertTrue(driver.findElement(By.cssSelector("input#register_phone")).isEnabled());
+        Assert.assertFalse(driver.findElement(By.cssSelector("input#register_phone_otp")).isEnabled());
+        Assert.assertFalse(driver.findElement(By.cssSelector("button.fhs-btn-register")).isEnabled());
 
-    // Element Selected: co the chon dc
-
-   }
-    @Test
-    public void TC_02_Enable() {
+        // Element Selected: da duoc chon
+        driver.get("https://live.techpanda.org/index.php/customer/account/create/");
+        Assert.assertTrue(driver.findElement(By.cssSelector("input#is_subscribed")).isDisplayed());//co hien thi
+        Assert.assertTrue(driver.findElement(By.cssSelector("input#is_subscribed")).isEnabled());//co the thao tac dc
+        Assert.assertFalse(driver.findElement(By.cssSelector("input#is_subscribed")).isSelected());//da tich chon
 
     }
+
     @Test
-    public void TC_03_Selected() {
+    public void TC_02_MailChimp() throws InterruptedException {
+        driver.get("https://login.mailchimp.com/signup/");
+        driver.findElement(By.cssSelector("input#email")).sendKeys("nhungnth2@gmail.com");
+        Thread.sleep(1000);
+        //sleep(1);dung ham san
+        //Chi nhap lowercase
+        driver.findElement(By.cssSelector("input#new_password")).sendKeys("test");
+        //driver.findElement(By.cssSelector("button#create-account-enabled")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.lowercase-char.completed")).isDisplayed());//Note: viet tat trong Css khoảng trắng trong value thay = dấu chấm
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.username-check.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.uppercase-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.special-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.number-char.not-completed")).isDisplayed());//Css co chua number thi dung format day du
+        Assert.assertTrue(driver.findElement(By.cssSelector("li[class='8-char not-completed']")).isDisplayed());
+        //Chi nhap upper case
+        driver.findElement(By.cssSelector("input#new_password")).clear();
+        driver.findElement(By.cssSelector("input#new_password")).sendKeys("TEST");
+        //driver.findElement(By.cssSelector("button#create-account-enabled")).click();
+        Thread.sleep(1000);
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.lowercase-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.username-check.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.uppercase-char.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.special-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li[class='8-char not-completed']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.number-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.number-char.not-completed")).isDisplayed());
+
+        //Chi nhap so
+        driver.findElement(By.cssSelector("input#new_password")).clear();
+        driver.findElement(By.cssSelector("input#new_password")).sendKeys("123456");
+        Thread.sleep(1000);
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.lowercase-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.username-check.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.uppercase-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.special-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li[class='8-char not-completed']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.number-char.completed")).isDisplayed());
+
+        //Chi nhap ky tu dac biet
+        driver.findElement(By.cssSelector("input#new_password")).clear();
+        driver.findElement(By.cssSelector("input#new_password")).sendKeys(".==-=");
+        Thread.sleep(1000);
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.lowercase-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.username-check.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.uppercase-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.special-char.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li[class='8-char not-completed']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.number-char.not-completed")).isDisplayed());
+
+
+        //Chi nhap 8 ky tu
+        driver.findElement(By.cssSelector("input#new_password")).clear();
+        driver.findElement(By.cssSelector("input#new_password")).sendKeys("Test0123");
+        Thread.sleep(1000);
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.lowercase-char.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.username-check.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.uppercase-char.completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.special-char.not-completed")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li[class='8-char completed']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.number-char.completed")).isDisplayed());
+
+        //Chi nhap hop le
+        driver.findElement(By.cssSelector("input#new_password")).clear();
+        driver.findElement(By.cssSelector("input#new_password")).sendKeys("Test@123");
+        Thread.sleep(1000);
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.lowercase-char.completed")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.uppercase-char.completed")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.special-char.completed")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("li[class='8-char completed']")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.number-char.completed")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.cssSelector("li.username-check.completed")).isDisplayed());
 
     }
-    @Test
-    public void TC_04_MailChimp() {
+    //Cach tao ham sleep(
+//public void sleep(long timeInSecond){
+//    try {
+//        Thread.sleep(timeInSecond*1000);
+//    } catch (InterruptedException e) {
+//        throw new RuntimeException(e);
+//    }
+//}
 
-    }
     @AfterClass
     public void afterClass() {
         driver.quit();
