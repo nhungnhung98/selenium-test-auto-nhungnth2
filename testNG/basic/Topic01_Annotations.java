636fd0a58d20fda80fda96226c1e96aaa1caf4eb
Nguyen Hong Nhung <hongnhung04091998@gmail.com>
2025-12-27 21:41:24 +0700

    testNG

---
+++ testNG/basic/Topic01_Annotations.java
@@ -0,0 +1,24 @@
+package basic;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Topic01_Annotations {
+
+    @BeforeClass
+    public static void beforeClass() {
+
+    }
+
+    @Test
+    public void loginToSystem() {
+
+    }
+
+    @AfterClass
+    public static void afterClass() {
+
+    }
+
+}
---
+++ testNG/basic/Topic02_Assert.java
@@ -0,0 +1,37 @@
+package basic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Topic02_Assert {
+    @Test
+    public void login(){
+        // Bắt buộc phải là kiểu dữ liệu boolean
+        // Selenium: tiền tố là isX: isDisplayed/ isEnable/ isSelected/ isMultiple
+        // Hàm tự define trả về boolean
+        // True: khi cần kiểm tra 1 dữ liệu trả về là đúng
+        // False: khi kiểm tra 1 dữ liệu trả về là sai
+
+        Assert.assertTrue(5 > 3);
+        boolean status =5 < 3;
+        System.out.println(status);
+        Assert.assertFalse(status);
+
+        //Equals: kiểm tra dữ liệu mong đợi với thực tế giống nhau
+        // 2 vế đều cùng 1 kiểu dữ liệu
+        // Kiểm tra về giá trị của dữ liệu và kiểu dữ liệu
+        String studentName ="Đặng Thuỳ Trâm";
+
+        Assert.assertEquals(studentName,"Đặng Thuỳ Trâm");
+        Assert.assertEquals(studentName,"Đặng Thuỳ trâm");
+
+        Object name="Đặng Thuỳ Trâm";
+
+        Assert.assertEquals(studentName, name);
+
+
+
+    }
+
+
+}
